be5f176d86c7e723a044a97e9326b65bc67829e5
Anna Troyan <ana.troyan@gmail.com>
2024-04-02 10:31:33 +0200

    Env values config

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
+import { VITE_API_URL } from "../env"
 
 
 function App() {
 
   return (
-    <>
       <div>
-      <h1>Hola</h1>
+      <h1>Hola  {VITE_API_URL}</h1>
 
       </div>
 
-    </>
   )
 }
 
